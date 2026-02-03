2f4730643b456343bb8752ff7a6e07dc67ff7a33
Rachit Dubey <rachitricky777@gmail.com>
2026-02-03 09:12:17 +0530

    Add pin configuration in ultrasonic sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <Arduino.h>
+#define triggerPin 9
+#define echoPin 10
 void setup() {
 // write your initialization code here
+    Serial.begin(9600);
+    pinMode(triggerPin, INPUT);
+    pinMode(echoPin, INPUT);
 }
 
 void loop() {
